2f858e172ef6da9f8fddf0e35039313ccccd84ed
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-01-28 21:35:21 +0100

    Added GroundChecker

---
+++ Team-Doe-N-Friends-Game/Assets/_Scripts/GroundChecker.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour{
+    [SerializeField] float groundCheckLenght = 1f;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask groundLayers;
+
+    public bool IsGrounded{
+        get;
+        private set;
+    }
+    void Update(){
+        GroundCheck();
+    }
+
+    /// <summary>
+    /// Casts a sphere from the transform's position downwards, dependant on radius, max distance and a layermask
+    /// </summary>
+    void GroundCheck(){
+        var ray = new Ray(transform.position, Vector3.down);
+        
+        //IsGrounded = Physics.SphereCast(ray, groundCheckRadius,groundCheckLenght, groundLayers);
+        IsGrounded = Physics2D.CircleCast(transform.position, groundCheckRadius, Vector3.down, groundCheckLenght,
+            groundLayers);
+        //Debug.DrawRay(transform.position,Vector3.down * groundCheckLenght, Color.magenta);
+    }
+
+    void OnDrawGizmos(){
+        Gizmos.DrawWireSphere(transform.position + Vector3.down * groundCheckLenght, groundCheckRadius);
+    }
+}
