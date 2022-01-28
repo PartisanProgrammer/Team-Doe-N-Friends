887044c142d81f8a6db5c78bd17bdfc3f84a262b
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-01-28 21:55:38 +0100

    Added PlayerJump Script

---
+++ Team-Doe-N-Friends-Game/Assets/_Scripts/Player Scripts/PlayerJump.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[DisallowMultipleComponent]
+[RequireComponent(typeof(PlayerInputs))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(GroundChecker))]
+public class PlayerJump : MonoBehaviour{
+
+   [Min(0)][SerializeField] float minJumpHeight;
+   [Min(0)][SerializeField] float maxJumpHeight;
+   [Min(0)][SerializeField] float jumpChargeTime;
+   
+   float jumpCharge;
+    
+    PlayerInputs playerInputs;
+    Rigidbody2D rigidbody2D;
+    GroundChecker groundChecker;
+    
+    void Awake(){
+        playerInputs = GetComponent<PlayerInputs>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        groundChecker = GetComponent<GroundChecker>();
+    }
+
+    void Update(){
+        //Jump
+        if (playerInputs.JumpInput){
+            //This value increases as long as player holds space
+            jumpCharge += Time.deltaTime / jumpChargeTime;
+        }
+
+        if (playerInputs.JumpInputUp){
+            if (groundChecker.IsGrounded){
+                //Lerps between min and max jump height.
+                var jumpForce = Mathf.Lerp(minJumpHeight, maxJumpHeight, jumpCharge);
+                rigidbody2D.AddForce(Vector2.up * jumpForce);
+            }
+            jumpCharge = 0f;
+        }
+    }
+}
