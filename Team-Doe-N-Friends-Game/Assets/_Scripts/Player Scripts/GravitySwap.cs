14c06486e6b76568800e3641cd557afc46d8c560
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-01-29 19:02:01 +0100

    Pushing unfinished Gravity Swap

--- Team-Doe-N-Friends-Game/Assets/_Scripts/Player Scripts/GravitySwap.cs
+++ Team-Doe-N-Friends-Game/Assets/_Scripts/Player Scripts/GravitySwap.cs
@@ -8,12 +8,19 @@ public class GravitySwap : MonoBehaviour{
 
     [SerializeField] float rotationDuration = 0.5f;
     [SerializeField] CharacterHolderSO characterHolderSo;
-    
-    float timeStart;
+    [SerializeField] AnimationCurve rotationCurve;
     float gravity = -1;
+    float rotationStart;
+    float currentRotation;
 
     public void SwitchGravity(){
-        transform.Rotate(Vector3.forward,180);
+        rotationStart = Time.time;
+        var timeLenght = Time.time + rotationDuration;
+        
+        float t = Mathf.InverseLerp(this.rotationStart, this.rotationStart + rotationDuration, Time.time); // time between 0 and 1
+        currentRotation = rotationCurve.Evaluate(t);
+        transform.rotation = Quaternion.Lerp(this.transform.rotation,new Quaternion(transform.rotation.x,transform.rotation.y,transform.rotation.z+ 180,transform.rotation.w),t);
+        transform.rotation = new Quaternion(0f,0f,currentRotation*180,0f);
         characterHolderSo.ChangeGravity();
         
     }
