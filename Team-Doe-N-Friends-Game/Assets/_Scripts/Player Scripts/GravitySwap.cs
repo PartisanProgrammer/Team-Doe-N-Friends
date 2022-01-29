c7f47f49e30f0a128c6b09bedef1d8cd4aa5124f
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2022-01-29 19:27:13 +0100

    gravity rotation revert

--- Team-Doe-N-Friends-Game/Assets/_Scripts/Player Scripts/GravitySwap.cs
+++ Team-Doe-N-Friends-Game/Assets/_Scripts/Player Scripts/GravitySwap.cs
@@ -2,25 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GravitySwap : MonoBehaviour{
-
-    [SerializeField] float rotationDuration = 0.5f;
+    
     [SerializeField] CharacterHolderSO characterHolderSo;
-    [SerializeField] AnimationCurve rotationCurve;
-    float gravity = -1;
-    float rotationStart;
-    float currentRotation;
 
     public void SwitchGravity(){
-        rotationStart = Time.time;
-        var timeLenght = Time.time + rotationDuration;
-        
-        float t = Mathf.InverseLerp(this.rotationStart, this.rotationStart + rotationDuration, Time.time); // time between 0 and 1
-        currentRotation = rotationCurve.Evaluate(t);
-        transform.rotation = Quaternion.Lerp(this.transform.rotation,new Quaternion(transform.rotation.x,transform.rotation.y,transform.rotation.z+ 180,transform.rotation.w),t);
-        transform.rotation = new Quaternion(0f,0f,currentRotation*180,0f);
+        transform.Rotate(Vector3.forward, 180);
         characterHolderSo.ChangeGravity();
         
     }
