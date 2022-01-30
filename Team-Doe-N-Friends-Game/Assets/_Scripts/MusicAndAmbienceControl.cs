7c99b4e9790a5a50643797df07649f2914cc71db
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2022-01-30 12:09:59 +0100

    stupid fix that solved the pausemanu sound bug

--- Team-Doe-N-Friends-Game/Assets/_Scripts/MusicAndAmbienceControl.cs
+++ Team-Doe-N-Friends-Game/Assets/_Scripts/MusicAndAmbienceControl.cs
@@ -41,6 +41,9 @@ public class MusicAndAmbienceControl : MonoBehaviour{
         if (playerRB == null){
             playerRB = FindObjectOfType<PlayerMovement>().GetComponent<Rigidbody2D>();
         }
+        if (pauseMenu == null){
+            pauseMenu = FindObjectOfType<PauseMenu>();
+        }
         if (playerRB.gravityScale == -1){
             ambienceInstance.setParameterByName("LightSide", 0);
             musicInstance.setParameterByName("LightSide", 0);
