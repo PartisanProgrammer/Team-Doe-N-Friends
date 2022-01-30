722575c4541dd04f741297634875712e2fab2f90
Erick Vaghi <vaghi.erick@gmail.com>
2022-01-30 14:26:11 +0100

    win script created

--- Team-Doe-N-Friends-Game/Assets/winGoal.cs
+++ Team-Doe-N-Friends-Game/Assets/winGoal.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.
+using UnityEngine.SceneManagement;
 
 public class winGoal : MonoBehaviour
 {
     void Update()
     {
-        
-    }
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
 }
