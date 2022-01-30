1819d61827b2b59612de46686cf25cdec2101207
Erick Vaghi <vaghi.erick@gmail.com>
2022-01-30 14:19:51 +0100

    Main menu bottom size fixerd

---
+++ Team-Doe-N-Friends-Game/Assets/winGoal.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.
+
+public class winGoal : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+}
