9e60dc3900747a72ecd5b661662671e17beade4c
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-01-30 01:02:04 +0100

    Added TrapReset time

--- Team-Doe-N-Friends-Game/Assets/_Scripts/Trap.cs
+++ Team-Doe-N-Friends-Game/Assets/_Scripts/Trap.cs
@@ -1,29 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class Trap : MonoBehaviour{
+    [SerializeField] float trapResetTime = 5;
+    [SerializeField] CinemachineImpulseSource impulseSource;
     GravitySwap gravitySwap;
     WorldSwitcher worldSwitcher;
+
+    bool canTrigger = true;
+    
 
     void Start(){
         gravitySwap = FindObjectOfType<GravitySwap>();
         worldSwitcher = FindObjectOfType<WorldSwitcher>();
+        impulseSource = GetComponent<CinemachineImpulseSource>();
     }
 
 
     void OnCollisionEnter2D(Collision2D collision){
-        if (collision.transform.CompareTag("Player")){
+        if (collision.transform.CompareTag("Player") && canTrigger){
             gravitySwap.SwitchGravity();
-                worldSwitcher.SwitchWorld();
+            worldSwitcher.SwitchWorld();
+            impulseSource.GenerateImpulse();
+            StartCoroutine(ResetTrap());
         }
     }
 
     void OnTriggerEnter2D(Collider2D col){
-        if (col.transform.CompareTag("Player")){
+        if (col.transform.CompareTag("Player") && canTrigger){
             gravitySwap.SwitchGravity();
             worldSwitcher.SwitchWorld();
+            impulseSource.GenerateImpulse();
+            StartCoroutine(ResetTrap());
         }
+    }
+
+    IEnumerator ResetTrap(){
+        canTrigger = false;
+        yield return new WaitForSeconds(trapResetTime);
+        canTrigger = true;
     }
 }
